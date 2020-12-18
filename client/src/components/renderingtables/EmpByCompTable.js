3ce71e189dcea482ff1316387c9d8b80a1b64469
Nheljee Rose Garcia <garcian5@mymacewan.ca>
2020-12-18 14:20:21 -0700

    created a new component that will render table for employees

---
+++ client/src/components/renderingtables/EmpByCompTable.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+export default class EmpByCompTable extends Component {
+  constructor () {
+    super();
+    this.state = {
+      company: {},
+      employees: [],
+      departments: []
+    }
+  }
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
